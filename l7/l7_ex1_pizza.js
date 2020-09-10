bddc08f39e5d4abacbe918786741dbbd0bcb4fe2
Dmytro Petrenko <dmitry.a.petrenko@gmail.com>
2020-09-10 11:54:11 +0300

    l7 ex1 pizza done

--- l7/l7_ex1_pizza.js
+++ l7/l7_ex1_pizza.js
@@ -7,66 +7,85 @@ function getPizzaForm() {
 
 function priceInit() {
   var basicPrice = document.getElementById("basicPrice");
-  var price = document.getElementById("price");
-  var pizza = getPizza();  
+  var priceElem = document.getElementById("price");
+  var price = 0;
+  var pizza = getPizza();
 
   if (pizza.pizzaSize === "Большая") {
     basicPrice.innerText = "200 грн";
+    price = 200;
   } else if (pizza.pizzaSize === "Средняя") {
     basicPrice.innerText = "150 грн";
+    price = 150;
   } else if (pizza.pizzaSize === "Маленькая") {
     basicPrice.innerText = "100 грн";
+    price = 100;
   }
+
+  for (let i = 0; i < pizza.inputs.length; i++) {
+    const input = pizza.inputs[i];
+    if (input.checked == true) {
+      if (input.labels[0].innerText.match("Сыр")) {
+        price += 30;
+      }
+      if (input.labels[0].innerText.match("Грибы")) {
+        price += 20;
+      }
+      if (input.labels[0].innerText.match("Бекон")) {
+        price += 50;
+      }
+      if (input.labels[0].innerText.match("Помидоры")) {
+        price += 15;
+      }
+      if (input.labels[0].innerText.match("Оливки")) {
+        price += 25;
+      }
+    }
+  }
+
+  priceElem.innerText = price + " грн";
 }
 
 function getPizza() {
   pizzaForm = getPizzaForm();
   var pizza = new Object();
+  pizza.inputs = new Array();
   for (let i = 0; i < pizzaForm.elements.length; i++) {
     const element = pizzaForm.elements[i];
     if (element.id == "selectPizzaSize") {
       pizza.pizzaSize = element.value;
+    }
+    if (element.id.match("additional-ingridients")) {
+      pizza.inputs.push(element);
     }
   }
   return pizza;
 }
 
 function init() {
+  var closeOrderCard = document.getElementById("closeOrderCard");
   var pizzaForm = getPizzaForm();
-  var flag = true;
 
   pizzaForm.addEventListener(
     "submit",
     function (event) {
-      if (pizzaForm.checkValidity() === false) {
-        flag = false;
-        event.preventDefault();
-        event.stopPropagation();
-      }
+      setOrderCardVisible(pizzaForm.checkValidity());
       pizzaForm.classList.add("was-validated");
       event.preventDefault();
     },
     false
   );
 
-  if (flag === true) {
-    var closeOrderCard = document.getElementById("closeOrderCard");
-
-    pizzaForm.addEventListener("submit", initOrder, false);
-    pizzaForm.addEventListener("submit", getOrderCard, false);
-    closeOrderCard.addEventListener("click", hideOrderCard, false);
-    getPizzaForm().addEventListener("change", priceInit, false);
-  }
+  pizzaForm.addEventListener("submit", initOrder, false);
+  getPizzaForm().addEventListener("change", priceInit, false);
 }
 
-function hideOrderCard() {
+function setOrderCardVisible(isValid) {
   orderCard = document.getElementById("orderCard");
-  orderCard.className = "container fixed-top invisible";
-}
-
-function getOrderCard() {
-  orderCard = document.getElementById("orderCard");
-  orderCard.className = "container fixed-top visible";
+  orderCard.className = `container fixed-top ${
+    isValid ? "visible" : "invisible"
+  }`;
+  getPizzaForm().classList.remove("was-validated");
 }
 
 function initOrder(e) {
@@ -107,6 +126,10 @@ function initOrder(e) {
       e.preventDefault();
     }
   }
+
+  document.getElementById("price-answer").innerHTML = document.getElementById(
+    "price"
+  ).innerHTML;
 
   e.preventDefault();
 }
